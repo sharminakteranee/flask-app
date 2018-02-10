086fa55519721d6b90cb9189c83c7ee474098905
sharminakteranee <anee051@hotmail.com>
2018-02-10 22:03:14 +0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/api/v1')
+def hello_world():
+    return 'Flask Dockerized new...........'
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=8080)
